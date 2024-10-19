c5ddc1ee6a382350f25d573f9865f9d8a3d2523a
Broady <broadybmx@gmail.com>
2024-10-19 11:30:09 -0400

    feat: Add toggleContrast function to change color theme for several elements

--- index.js
+++ index.js
@@ -4,6 +4,20 @@
 
 //User ID: 6G_F_yg7CUVy-kc9W
 
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
@@ -28,8 +42,6 @@ function contact(event) {
       );
     });
 }
-
-let isModalOpen = false;
 
 function toggleModal() {
   // toggle modal
